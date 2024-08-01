6a0431de75c0057ef9760986e5a971b99abc8793
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2024-08-01 09:04:41 +0900

    Fix missing variable

--- build.rb
+++ build.rb
@@ -5,8 +5,6 @@ require 'fileutils'
 
 archs = %w[amd64 arm64 armhf]
 
-puts "Build #{v} snap package"
-
 v = ARGV[0] || '3.3.4'
 abi_v = v.split(".").tap{|v| v[-1] = "0"}.join(".")
 series_v = abi_v[0..2]
@@ -14,4 +12,5 @@ series_v = abi_v[0..2]
 yml = ERB.new(File.read("snap/local/snapcraft.yaml.erb")).result(binding)
 File.open("snap/snapcraft.yaml", "w") {|f| f.puts yml }
 
-system "snapcraft remote-build --build-on=#{arch.join(",")} --launchpad-accept-public-upload"
+puts "Build #{v} snap package"
+system "snapcraft remote-build --build-on=#{archs.join(",")} --launchpad-accept-public-upload"
